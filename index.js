ffce43a9326f4dd3b60b9fdee68d4cc9e51e79c7
mrityunjay45108 <kumarmrityunjay5210@gmail.com>
2026-04-09 19:17:01 +0530

    database connect

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-import express from 'express';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
-
+dotenv.config();
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// cors
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Welcome to the backend!",
+    timestamp: new Date().toISOString(),
+    success: true
+  });
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
 });
